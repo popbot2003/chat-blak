a99118b663c0489b262ad3f8a49aaea39e7dd48c
popbot2003 <hussienatef707@gmail.com>
2026-06-07 09:28:07 +0300

    Refactor TypingDots to use empty spans
    Updated TypingDots component to simplify dot rendering.

--- src/components/TypingDots.jsx
+++ src/components/TypingDots.jsx
@@ -1,9 +1,9 @@
 export default function TypingDots() {
   return (
     <div className="typing-dots">
-      <span className="typing-dot" style={{ animationDelay: "0ms" }} />
-      <span className="typing-dot" style={{ animationDelay: "150ms" }} />
-      <span className="typing-dot" style={{ animationDelay: "300ms" }} />
+      <span />
+      <span />
+      <span />
     </div>
   );
 }
