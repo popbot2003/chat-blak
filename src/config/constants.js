ad84bec9beb90c0b8330653ac08dd127b2d26d76
popbot2003 <hussienatef707@gmail.com>
2026-06-11 11:12:43 +0300

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -10,7 +10,7 @@ export const DEFAULT_USER_DAILY_LIMIT = 30000;
 export const DEFAULT_KEY_DAILY_LIMIT = 1000000;
 
 // إعدادات Groq API
-export const GROQ_MODEL = "deepseek-r1-distill-qwen-32b";
+export const GROQ_MODEL = "openai/gpt-oss-120b";
 export const GROQ_MAX_TOKENS = 2000;
 export const GROQ_TEMPERATURE = 0.3;
 
